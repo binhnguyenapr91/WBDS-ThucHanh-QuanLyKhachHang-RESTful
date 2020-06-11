eccb73bf5138fa819398895bc4f9f85979571511
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-06-12 00:02:28 +0700

    init project

---
+++ src/main/java/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+package service.impl;
+
+import model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import repository.CustomerRepository;
+import service.CustomerService;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.remove(id);
+    }
+}
